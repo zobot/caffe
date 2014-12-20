47b31c827e0166aa5b10f8ded60992940da52862
Chelsea Finn <cbfinn@eecs.berkeley.edu>
2014-12-20 14:36:18 -0800

    added forward to caffe cpp script for hdf file

--- tools/caffe.cpp
+++ tools/caffe.cpp
@@ -1,11 +1,16 @@
 #include <glog/logging.h>
 
+#include <fstream>  // NOLINT(readability/streams)
 #include <cstring>
 #include <map>
 #include <string>
 #include <vector>
 
+#include "hdf5.h"
+#include "hdf5_hl.h"
 #include "caffe/caffe.hpp"
+#include "caffe/data_layers.hpp"
+#include "caffe/util/io.hpp"
 
 using caffe::Blob;
 using caffe::Caffe;
@@ -14,6 +19,7 @@ using caffe::Layer;
 using caffe::shared_ptr;
 using caffe::Timer;
 using caffe::vector;
+using caffe::MemoryDataLayer;
 
 
 DEFINE_int32(gpu, -1,
@@ -22,6 +28,8 @@ DEFINE_string(solver, "",
     "The solver definition protocol buffer text file.");
 DEFINE_string(model, "",
     "The model definition protocol buffer text file..");
+DEFINE_string(data, "",
+    "The file containing data.");
 DEFINE_string(snapshot, "",
     "Optional; the snapshot solver state to resume training.");
 DEFINE_string(weights, "",
@@ -190,6 +198,143 @@ int test() {
 }
 RegisterBrewFunction(test);
 
+// Forward: Run a forward pass.
+int forward() {
+  CHECK_GT(FLAGS_model.size(), 0) << "Need a model definition to score.";
+  CHECK_GT(FLAGS_weights.size(), 0) << "Need model weights to score.";
+  CHECK_GT(FLAGS_data.size(), 0) << "Need data to read.";
+
+  // Set device id and mode
+  if (FLAGS_gpu >= 0) {
+    LOG(INFO) << "Use GPU with device ID " << FLAGS_gpu;
+    Caffe::SetDevice(FLAGS_gpu);
+    Caffe::set_mode(Caffe::GPU);
+  } else {
+    LOG(INFO) << "Use CPU.";
+    Caffe::set_mode(Caffe::CPU);
+  }
+  // Instantiate the caffe net.
+  Caffe::set_phase(Caffe::TEST);
+  Net<float> caffe_net(FLAGS_model);
+  caffe_net.CopyTrainedLayersFrom(FLAGS_weights);
+
+  // Get some data to use
+  DLOG(INFO) << "Loading HDF5 file: " << FLAGS_data;
+  hid_t file_id = H5Fopen(FLAGS_data.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
+  if (file_id < 0) {
+    LOG(FATAL) << "Failed opening HDF5 file: " << FLAGS_data;
+  }
+  const int MIN_DATA_DIM = 1;
+  const int MAX_DATA_DIM = 4;
+  std::vector<shared_ptr<Blob<float> > > hdf_blobs_;
+  hdf_blobs_.resize(4);
+  hdf_blobs_[0] = shared_ptr<Blob<float> >(new Blob<float>());
+  hdf_blobs_[1] = shared_ptr<Blob<float> >(new Blob<float>());
+  hdf_blobs_[2] = shared_ptr<Blob<float> >(new Blob<float>());
+  hdf_blobs_[3] = shared_ptr<Blob<float> >(new Blob<float>());
+  caffe::hdf5_load_nd_dataset(file_id, "rgb_frames", MIN_DATA_DIM, MAX_DATA_DIM, hdf_blobs_[0].get());
+  caffe::hdf5_load_nd_dataset(file_id, "joint_states", MIN_DATA_DIM, MAX_DATA_DIM, hdf_blobs_[1].get());
+  caffe::hdf5_load_nd_dataset(file_id, "output_torques", MIN_DATA_DIM, MAX_DATA_DIM, hdf_blobs_[2].get());
+  caffe::hdf5_load_nd_dataset(file_id, "loss_weights", MIN_DATA_DIM, MAX_DATA_DIM, hdf_blobs_[3].get());
+  H5Fclose(file_id);
+
+  // Load data.
+  shared_ptr<MemoryDataLayer<float> > md_layer =
+    boost::dynamic_pointer_cast<MemoryDataLayer<float> >(caffe_net.layers()[0]);
+  md_layer->Reset((hdf_blobs_[0])->mutable_cpu_data(),
+                  hdf_blobs_[1]->mutable_cpu_data(),
+                  hdf_blobs_[2]->mutable_cpu_data(),
+                  hdf_blobs_[3]->mutable_cpu_data(), 1000);
+
+  vector<Blob<float>* > bottom_vec;
+  vector<int> test_score_output_id;
+  vector<float> test_score;
+
+
+
+  // Do a clean forward and backward pass, so that memory allocation are done
+  // and future iterations will be more stable.
+  LOG(INFO) << "Performing Forward";
+  float initial_loss;
+  caffe_net.Forward(vector<Blob<float>*>(), &initial_loss);
+  LOG(INFO) << "Initial loss: " << initial_loss;
+  LOG(INFO) << "Performing Backward";
+  caffe_net.Backward();
+/*
+  const vector<shared_ptr<Layer<float> > >& layers = caffe_net.layers();
+  vector<vector<Blob<float>*> >& bottom_vecs = caffe_net.bottom_vecs();
+  vector<vector<Blob<float>*> >& top_vecs = caffe_net.top_vecs();
+  const vector<vector<bool> >& bottom_need_backward =
+      caffe_net.bottom_need_backward();
+  LOG(INFO) << "*** Benchmark begins ***";
+  LOG(INFO) << "Testing for " << FLAGS_iterations << " iterations.";
+  Timer total_timer;
+  total_timer.Start();
+  Timer forward_timer;
+  Timer backward_timer;
+  Timer timer;
+  std::vector<double> forward_time_per_layer(layers.size(), 0.0);
+  std::vector<double> backward_time_per_layer(layers.size(), 0.0);
+  double forward_time = 0.0;
+  double backward_time = 0.0;
+  for (int j = 0; j < FLAGS_iterations; ++j) {
+    Timer iter_timer;
+    iter_timer.Start();
+    forward_timer.Start();
+    for (int i = 0; i < layers.size(); ++i) {
+      timer.Start();
+      // Although Reshape should be essentially free, we include it here
+      // so that we will notice Reshape performance bugs.
+      layers[i]->Reshape(bottom_vecs[i], top_vecs[i]);
+
+      if (bottom_vecs[i].size() != 0) LOG(INFO) << "Layer "<<i<<", Params: "<<bottom_vecs[i][0]->count();
+      layers[i]->Forward(bottom_vecs[i], top_vecs[i]);
+      forward_time_per_layer[i] += timer.MicroSeconds();
+    }
+    forward_time += forward_timer.MicroSeconds();
+
+
+
+  float loss = 0;
+  float iter_loss;
+  const vector<Blob<float>*>& result =
+      caffe_net.Forward(bottom_vec, &iter_loss);
+  loss += iter_loss;
+  int idx = 0;
+  for (int j = 0; j < result.size(); ++j) {
+    const float* result_vec = result[j]->cpu_data();
+    for (int k = 0; k < result[j]->count(); ++k, ++idx) {
+      const float score = result_vec[k];
+      if (i == 0) {
+        test_score.push_back(score);
+        test_score_output_id.push_back(j);
+      } else {
+        test_score[idx] += score;
+      }
+      const std::string& output_name = caffe_net.blob_names()[
+          caffe_net.output_blob_indices()[j]];
+      LOG(INFO) << "Batch " << i << ", " << output_name << " = " << score;
+    }
+  }
+  LOG(INFO) << "Loss: " << loss;
+  for (int i = 0; i < test_score.size(); ++i) {
+    const std::string& output_name = caffe_net.blob_names()[
+        caffe_net.output_blob_indices()[test_score_output_id[i]]];
+    const float loss_weight =
+        caffe_net.blob_loss_weights()[caffe_net.output_blob_indices()[i]];
+    std::ostringstream loss_msg_stream;
+    const float mean_score = test_score[i] / FLAGS_iterations;
+    if (loss_weight) {
+      loss_msg_stream << " (* " << loss_weight
+                      << " = " << loss_weight * mean_score << " loss)";
+    }
+    LOG(INFO) << output_name << " = " << mean_score << loss_msg_stream.str();
+  }
+*/
+  return 0;
+}
+RegisterBrewFunction(forward);
+
 
 // Time: benchmark the execution time of a model.
 int time() {
@@ -244,6 +389,8 @@ int time() {
       // Although Reshape should be essentially free, we include it here
       // so that we will notice Reshape performance bugs.
       layers[i]->Reshape(bottom_vecs[i], top_vecs[i]);
+
+      if (bottom_vecs[i].size() != 0) LOG(INFO) << "Layer "<<i<<", Params: "<<bottom_vecs[i][0]->count();
       layers[i]->Forward(bottom_vecs[i], top_vecs[i]);
       forward_time_per_layer[i] += timer.MicroSeconds();
     }
