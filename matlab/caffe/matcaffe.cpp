f16ca3100653335015544730a728f62ee3ea9be3
Chelsea Finn <cbfinn@eecs.berkeley.edu>
2014-12-29 18:56:50 -0800

    reset solver in matcaffe

--- matlab/caffe/matcaffe.cpp
+++ matlab/caffe/matcaffe.cpp
@@ -537,6 +537,9 @@ static void init_test(MEX_ARGS) {
 
   char* param_file = mxArrayToString(prhs[0]);
 
+  if (solver_) {
+    solver_.reset();
+  }
   net_.reset(new Net<float>(string(param_file)));
   if (nrhs == 2) {
     char* model_file = mxArrayToString(prhs[1]);
