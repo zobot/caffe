b14cf40101d598e51340d203907e2b517ff514da
Zoe McCarthy <zoemccarthy12@gmail.com>
2015-03-16 21:36:46 -0500

    can compile the spatial softmax layer now

--- include/caffe/vision_layers.hpp
+++ include/caffe/vision_layers.hpp
@@ -418,6 +418,45 @@ class PoolingLayer : public Layer<Dtype> {
   Blob<int> max_idx_;
 };
 
+/**
+ * @brief Computes the softmax function.
+ *
+ * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+ */
+template <typename Dtype>
+class SpatialSoftmaxLayer : public Layer<Dtype> {
+ public:
+  explicit SpatialSoftmaxLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "SpatialSoftmax"; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  /// sum_multiplier is used to carry out sum using BLAS
+  Blob<Dtype> sum_multiplier_;
+  /// scale is an intermediate Blob to hold temporary results.
+  Blob<Dtype> scale_;
+
+  // spatial specific data
+  Blob<Dtype> temp_data_;
+  Dtype temp_;
+  string dimension_;
+};
+
+
 #ifdef USE_CUDNN
 /*
  * @brief cuDNN implementation of PoolingLayer.
