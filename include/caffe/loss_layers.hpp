9e3f74ac9811a441303a35bd5812aa0e68eb43bd
Chelsea Finn <cbfinn@eecs.berkeley.edu>
2015-03-21 15:17:35 -0700

    added distance and weighted euclidean loss layer

--- include/caffe/loss_layers.hpp
+++ include/caffe/loss_layers.hpp
@@ -78,13 +78,68 @@ class AccuracyLayer : public Layer<Dtype> {
     }
   }
 
-  /// Whether to ignore instances with a certain label.
+  /// Whether to ignore instances with a certain label
   bool has_ignore_label_;
   /// The label indicating that an instance should be ignored.
   int ignore_label_;
 
   int top_k_;
   Dtype denominator_;
+};
+
+/**
+ * @brief Computes the average distance for a position estimation
+ *        task.
+ */
+template <typename Dtype>
+class EuclideanDistanceLayer : public Layer<Dtype> {
+ public:
+  explicit EuclideanDistanceLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "EuclideanDistance"; }
+
+  virtual inline int ExactNumBottomBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  /**
+   * @param bottom input Blob vector (length 2)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the predictions @f$ x @f$, a Blob with values in
+   *      @f$ [-\infty, +\infty] @f$ indicating the predicted score for each of
+   *      the @f$ K = CHW @f$ classes. Each @f$ x_n @f$ is mapped to a predicted
+   *      label @f$ \hat{l}_n @f$ given by its maximal index:
+   *      @f$ \hat{l}_n = \arg\max\limits_k x_{nk} @f$
+   *   -# @f$ (N \times 1 \times 1 \times 1) @f$
+   *      the labels @f$ l @f$, an integer-valued Blob with values
+   *      @f$ l_n \in [0, 1, 2, ..., K - 1] @f$
+   *      indicating the correct class label among the @f$ K @f$ classes
+   * @param top output Blob vector (length 1)
+   *   -# @f$ (1 \times 1 \times 1 \times 1) @f$
+   *      the computed accuracy: @f$
+   *        \frac{1}{N} \sum\limits_{n=1}^N \delta\{ \hat{l}_n = l_n \}
+   *      @f$, where @f$
+   *      \delta\{\mathrm{condition}\} = \left\{
+   *         \begin{array}{lr}
+   *            1 & \mbox{if condition} \\
+   *            0 & \mbox{otherwise}
+   *         \end{array} \right.
+   *      @f$
+   */
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+
+  /// @brief Not implemented -- AccuracyLayer cannot be used as a loss.
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+    for (int i = 0; i < propagate_down.size(); ++i) {
+      if (propagate_down[i]) { NOT_IMPLEMENTED; }
+    }
+  }
 };
 
 /**
@@ -298,6 +353,103 @@ class EuclideanLossLayer : public LossLayer<Dtype> {
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 
   Blob<Dtype> diff_;
+};
+
+/**
+ * @brief Computes the Weighted Euclidean (L2) loss @f$
+ *          E = \frac{1}{2N} \sum\limits_{n=1}^N (\hat{y}_n - y_n)^T * A *
+ *          (\hat{y}_n - y_n) @f$ for real-valued regression tasks.
+ *
+ * @param bottom input Blob vector (length 3)
+ *   -# @f$ (N \times C \times 1 \times 1) @f$
+ *      the predictions @f$ \hat{y} \in [-\infty, +\infty]@f$
+ *   -# @f$ (N \times C \times 1 \times 1) @f$
+ *      the targets @f$ y \in [-\infty, +\infty]@f$
+ *   -# @f$ (N \times C \times C \times 1) @f$
+ *      the weight matrix
+ * @param top output Blob vector (length 1)
+ *   -# @f$ (1 \times 1 \times 1 \times 1) @f$
+ *      the computed Weighted Euclidean loss: @f$ E =
+ *          \frac{1}{2n} \sum\limits_{n=1}^N \left| \left| \hat{y}_n - y_n
+ *        \right| \right|_2^2 @f$
+ *
+ * This can be used for least-squares regression tasks.  An InnerProductLayer
+ * input to a EuclideanLossLayer exactly formulates a linear least squares
+ * regression problem. With non-zero weight decay the problem becomes one of
+ * ridge regression -- see src/caffe/test/test_sgd_solver.cpp for a concrete
+ * example wherein we check that the gradients computed for a Net with exactly
+ * this structure match hand-computed gradient formulas for ridge regression.
+ *
+ * (Note: Caffe, and SGD in general, is certainly \b not the best way to solve
+ * linear least squares problems! We use it only as an instructive example.)
+ */
+template <typename Dtype>
+class WeightedEuclideanLossLayer : public LossLayer<Dtype> {
+ public:
+  explicit WeightedEuclideanLossLayer(const LayerParameter& param)
+      : LossLayer<Dtype>(param), diff_() {}
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+	// WeightedEuclideanLossLayer takes 3 Blobs. The third should be the weights
+	// matrix for each data sample. In the future, there should be an option to
+	// load the weights matrix from a file specified by LayerParameter.
+  virtual inline int ExactNumBottomBlobs() const { return 3; }
+  virtual inline const char* type() const { return "WeightedEuclideanLoss"; }
+
+  /**
+   * Unlike most loss layers, in the WeightedEuclideanLossLayer we can backpropagate
+   * to both inputs -- override to return true and always allow force_backward.
+   * However, it only has this ability on the first two blobs and there is
+   * currently no functionality to specify which bottom blobs you can force
+   * backward computation on.
+   */
+  virtual inline bool AllowForceBackward(const int bottom_index) const {
+    return bottom_index != 2;
+  }
+
+ protected:
+  /// @copydoc WeightedEuclideanLossLayer
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  /**
+   * @brief Computes the Weighted Euclidean error gradient w.r.t. the inputs.
+   *
+   * Unlike other children of LossLayer, WeightedEuclideanLossLayer \b can compute
+   * gradients with respect to the label inputs bottom[1] (but still only will
+   * if propagate_down[1] is set, due to being produced by learnable parameters
+   * or if force_backward is set). In fact, this layer is "commutative" -- the
+   * result is the same regardless of the order of the two bottoms.
+   *
+   * @param top output Blob vector (length 1), providing the error gradient with
+   *      respect to the outputs
+   *   -# @f$ (1 \times 1 \times 1 \times 1) @f$
+   *      This Blob's diff will simply contain the loss_weight* @f$ \lambda @f$,
+   *      as @f$ \lambda @f$ is the coefficient of this layer's output
+   *      @f$\ell_i@f$ in the overall Net loss
+   *      @f$ E = \lambda_i \ell_i + \mbox{other loss terms}@f$; hence
+   *      @f$ \frac{\partial E}{\partial \ell_i} = \lambda_i @f$.
+   *      (*Assuming that this top Blob is not used as a bottom (input) by any
+   *      other layer of the Net.)
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length 3)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the predictions @f$\hat{y}@f$; Backward fills their diff with
+   *      gradients @f$
+   *        \frac{\partial E}{\partial \hat{y}} =
+   *            \frac{1}{n} \sum\limits_{n=1}^N (\hat{y}_n - y_n)
+   *      @f$ if propagate_down[0]
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the targets @f$y@f$; Backward fills their diff with gradients
+   *      @f$ \frac{\partial E}{\partial y} =
+   *          \frac{1}{n} \sum\limits_{n=1}^N (y_n - \hat{y}_n)
+   *      @f$ if propagate_down[1]
+   */
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  Blob<Dtype> diff_, temp_;
 };
 
 /**
---
+++ src/caffe/layers/weighted_euclidean_loss_layer.cpp
@@ -0,0 +1,81 @@
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/util/io.hpp"
+#include "caffe/util/math_functions.hpp"
+#include "caffe/vision_layers.hpp"
+
+namespace caffe {
+
+// bottom[0] is the output of the network, bottom[1] is the output_torque,
+// bottom[2] is the precision matrix * lambda_t of weights for each time
+template <typename Dtype>
+void WeightedEuclideanLossLayer<Dtype>::Reshape(
+  const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+  LossLayer<Dtype>::Reshape(bottom, top);
+  CHECK_EQ(bottom[0]->channels(), bottom[1]->channels());
+  CHECK_EQ(bottom[0]->height(), bottom[1]->height());
+  CHECK_EQ(bottom[0]->width(), bottom[1]->width());
+  CHECK_EQ(bottom[0]->height(), 1);
+  CHECK_EQ(bottom[0]->width(), 1);
+  CHECK_EQ(bottom[2]->width(), 1);
+  CHECK_EQ(bottom[2]->num(), bottom[0]->num());
+  CHECK_EQ(bottom[2]->channels(), bottom[2]->height());
+  CHECK_EQ(bottom[2]->height(), bottom[0]->channels());
+  diff_.Reshape(bottom[0]->num(), bottom[0]->channels(), 1, 1);
+  temp_.Reshape(bottom[0]->num(), bottom[2]->channels(), 1, 1);
+}
+
+template <typename Dtype>
+void WeightedEuclideanLossLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+    const vector<Blob<Dtype>*>& top) {
+  int count = bottom[0]->count();
+  int channels = bottom[2]->channels();
+  int height = bottom[2]->height();
+  Dtype loss(0.0), dot(0.0), inner_dot(0.0);
+  caffe_sub(
+      count,
+      bottom[0]->cpu_data(),
+      bottom[1]->cpu_data(),
+      diff_.mutable_cpu_data());
+  for (int i = 0; i < bottom[0]->num(); ++i) {
+    dot = 0.0;
+    for (int row = 0; row < channels; ++row) {
+      inner_dot = 0.0;
+      for (int k = 0; k < height; ++k) {
+        inner_dot += bottom[2]->data_at(i, row, k, 0) * diff_.data_at(i, k, 0, 0);
+      }
+      dot += diff_.data_at(i, row, 0, 0) * inner_dot;
+    }
+    loss += dot;
+  }
+  top[0]->mutable_cpu_data()[0] = loss / bottom[0]->num() / Dtype(2);
+}
+
+template <typename Dtype>
+void WeightedEuclideanLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
+    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+  for (int i = 0; i < 2; ++i) {
+    if (propagate_down[i]) {
+      const Dtype sign = (i == 0) ? 1. : -1.;
+      const Dtype alpha = sign * top[0]->cpu_diff()[0] / bottom[i]->num();
+      Dtype* bottom_diff = bottom[i]->mutable_cpu_diff();
+
+      for (int n = 0; n < bottom[i]->num(); ++n) {
+        for (int row = 0; row < bottom[2]->channels(); ++row) {
+          Dtype temp = 0;
+          for (int k = 0; k < bottom[2]->height(); ++k) {
+            temp += bottom[2]->data_at(n, row, k, 0) * diff_.data_at(n, k, 0, 0);
+          }
+          *(bottom_diff + bottom[i]->offset(n, row)) = temp;
+        }
+      }
+      caffe_scal(bottom[i]->count(), alpha, bottom_diff);
+    }
+  }
+}
+
+INSTANTIATE_CLASS(WeightedEuclideanLossLayer);
+REGISTER_LAYER_CLASS(WeightedEuclideanLoss);
+}  // namespace caffe
